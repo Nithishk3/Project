37014e9ab980b8188a8d756ce4b25206d0996d3d
Nithishk3 <86427199+Nithishk3@users.noreply.github.com>
2022-02-18 10:19:30 +0530

    Create merchant.java

---
+++ Amazon/merchant.java
@@ -0,0 +1,126 @@
+package Amazon;
+import java.util.*;
+class Merchant {
+    String username;
+    String id;
+    int pin;
+    String status;
+    Merchant(String a,String b,int c,String d){
+        this.username=a;
+        this.id=b;
+        this.pin=c;
+        this.status=d;
+    }
+    Merchant(){}
+    static int b=101;
+    static List<Merchant>ls=new ArrayList<>();
+    Scanner sc;
+    void mainpage(){
+        ls.add(new Merchant("nithish","m100",1234,"ok"));
+        sc=new Scanner(System.in);
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();
+        while(true){
+            System.out.println("\t Welcome Merchant");
+            System.out.println("1:New Merchant\n2:Existing Merchant\n3:Back");
+            System.out.print("Enter your choice : ");
+            int a=sc.nextInt();
+            switch(a){
+                case 1:
+                     newm();
+                    break;
+                case 2:
+                    existing();
+                    break;
+                case 3:
+                    Amazon z=new Amazon();
+                    z.mainfun();
+                    break;
+                default:
+                    System.out.print("\033[H\033[2J");  
+                    System.out.flush();
+                    System.out.println("Invalid input!!..");
+                    break;
+            }
+        }
+    }
+    void newm(){
+        System.out.print("\033[H\033[2J");
+        sc.nextLine();
+        System.out.print("Enter your username : ");
+        String a=sc.nextLine();
+        System.out.print("Enter your password : ");
+        int c=sc.nextInt();
+        sc.nextLine();
+        int h=b++;
+        String B="M"+h;
+        ls.add(new Merchant(a,B,c,"no"));
+        System.out.println("user is wating for approval!!.");
+        System.out.println("your id : "+B);
+        System.out.println("Press enter to continue....");
+        sc.nextLine();
+        Merchant z=new Merchant();
+        z.mainpage();
+    }
+    int findapp(int v){
+        if(ls.get(v).status.equals("ok")){
+            return 1;
+        }
+        else if(ls.get(v).status.equals("Rejected")){
+            return 2;
+        }
+        return 3;
+    }
+    void existing(){
+        System.out.print("\033[H\033[2J");
+        sc.nextLine();
+        int k=-1;
+        boolean fla=false;
+        System.out.print("Enter your user id : ");
+        String id=sc.nextLine();
+        for(int i=0;i<ls.size();i++){
+            if(ls.get(i).id.equals(id)){
+                k=i;
+                fla=true;
+                break;
+            }
+        }
+        if(fla){
+            System.out.print("Enter your password : ");
+            int pass=sc.nextInt();
+            if(ls.get(k).pin==pass){
+            if(ls.get(k).id.equals(id)&&findapp(k)==1){
+                Product x=new Product();
+                x.pmainpage(k);
+            }else if(findapp(k)==2){
+                System.out.println("Your account is banned \n please contact the admin !!!");
+                System.out.println("Press enter to continue..... ");
+                sc.nextLine();
+                sc.nextLine();
+                Merchant z=new Merchant();
+                z.mainpage();
+            }else{
+                System.out.println("Wating for approval");
+                System.out.println("Press enter to continue ");
+                sc.nextLine();
+                sc.nextLine();
+                Merchant z=new Merchant();
+                z.mainpage();
+            }
+        }else{
+            System.out.println("Enter pin correctly");
+            System.out.println("press enter to continue ");
+            sc.nextLine();
+            sc.nextLine();
+            Merchant z=new Merchant();
+            z.mainpage();
+        }
+        }else{
+            System.out.println("Merchant not found!!..");
+            System.out.println("press enter to continue ");
+            sc.nextLine();
+            Merchant z=new Merchant();
+            z.mainpage();
+        }
+    }
+}
